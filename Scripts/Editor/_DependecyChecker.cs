efbd35ae7c0e573964c358e8a15ac3d0a0e8ce44
rurre <sergiuung@msn.com>
2019-09-15 22:41:31 +0100

    Update _DependecyChecker.cs

--- Scripts/Editor/_DependecyChecker.cs
+++ Scripts/Editor/_DependecyChecker.cs
@@ -3,17 +3,27 @@ using UnityEditor;
 using System;
 using System.IO;
 
-namespace Pumkin
+namespace Pumkin.DependencyChecker
 {
     [InitializeOnLoad]
     public class _DependecyChecker
     {
+        static readonly float checkCooldown = 5f;
+
         static string noBones = "#define NO_BONES\r\n";
         static string oldBones = "#define OLD_BONES\r\n";
-        
+
+        public static string MainScriptPath { get; private set; }
+        public enum CheckerStatus { NO_BONES, OLD_BONES, NO_SDK, OK, DEFAULT };
+
+        public static CheckerStatus Status { get; private set; }
+
+        static float lastCheck = 0;
+                
         static _DependecyChecker()
         {
-            Check();
+            Status = CheckerStatus.DEFAULT;
+            Check();  
         }        
 
         [UnityEditor.Callbacks.DidReloadScripts]
@@ -24,9 +34,36 @@ namespace Pumkin
 
         public static void Check()
         {
+            if(Status != CheckerStatus.DEFAULT)
+            {
+                float now = Time.time;
+                if(now >= lastCheck + checkCooldown)
+                    lastCheck = now;
+                else
+                    return;
+            }
+
+            Debug.Log("<color=blue>PumkinsAvatarTools</color>: Checking for VRChat SDK in project...");
+            Type sdkType = GetType("VRCSDK2.VRC_AvatarDescriptor");
+
+            if(sdkType == null)
+            {
+                Debug.Log("<color=blue>PumkinsAvatarTools</color>: VRChat SDK not found. Please import the SDK to use these tools.");
+                Status = CheckerStatus.NO_SDK;
+                return;
+            }
+            else
+            {
+                Debug.Log("<color=blue>PumkinsAvatarTools</color>: Found VRChat SDK.");
+                Status = CheckerStatus.OK;
+            }
+
+
+            Debug.Log("<color=blue>PumkinsAvatarTools</color>: Checking for DynamicBones in project...");
             Type boneType = GetType("DynamicBoneCollider");
-            Type toolsType = GetType("Pumkin.PumkinsAvatarTools");
-            var toolScriptPath = Directory.GetFiles(Application.dataPath, "PumkinsAvatarTools.cs", SearchOption.AllDirectories);
+            Type toolsType = GetType("Pumkin.AvatarTools.PumkinsAvatarTools");            
+
+            var toolScriptPath = Directory.GetFiles(Application.dataPath, "PumkinsAvatarTools.cs", SearchOption.AllDirectories);            
 
             if(toolScriptPath.Length > 0 && !string.IsNullOrEmpty(toolScriptPath[0]))
             {
@@ -46,6 +83,9 @@ namespace Pumkin
 
                         File.WriteAllText(toolScriptPath[0], header + toolsFile);
                         AssetDatabase.ImportAsset(RelativePath(toolScriptPath[0]));
+
+                        Status = CheckerStatus.NO_BONES;
+                        Debug.Log("<color=blue>PumkinsAvatarTools</color>: DynamicBones not found in project");
                     }
                 }
                 else //DynamicBones Present
@@ -68,6 +108,9 @@ namespace Pumkin
 
                                 File.WriteAllText(toolScriptPath[0], header + toolsFile);
                                 AssetDatabase.ImportAsset(RelativePath(toolScriptPath[0]));
+
+                                Status = CheckerStatus.OLD_BONES;
+                                Debug.Log("<color=blue>PumkinsAvatarTools</color>: Found old version of Dynamic Bones in project");
                             }                            
                         }
                         else //New Version of DynamicBone
@@ -76,7 +119,13 @@ namespace Pumkin
                             {
                                 File.WriteAllText(toolScriptPath[0], toolsFile);
                                 AssetDatabase.ImportAsset(RelativePath(toolScriptPath[0]));
+
+                                Status = CheckerStatus.OK;                                
                             }
+                        }
+                        if(Status == CheckerStatus.OK)
+                        {
+                            Debug.Log("<color=blue>PumkinsAvatarTools</color>: Found Dynamic Bones in project");
                         }
                     }
                 }
@@ -97,7 +146,7 @@ namespace Pumkin
             return null;
         }
 
-        static string RelativePath(string path)
+        public static string RelativePath(string path)
         {            
             if(path.StartsWith(Application.dataPath))
             {
